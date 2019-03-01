aff350e612d7049c886d0aa341ae9f9b4ce577c9
Lars Larsen <lal@jyskebank.dk>
2019-03-01 14:19:38 +0100

    added opgave 2.1

---
+++ uge2/Opgave2_1/Opgave2_1.console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Opgave2_1.console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
